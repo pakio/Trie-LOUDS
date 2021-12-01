ce3d93a536d125744dbfc6b9a02d1e50e5dc52f5
pakiosann@gmail.com <pakiosann@gmail.com>
2021-12-01 15:09:40 +0900

    add license and memory measurement

--- src/main/java/com/pakio/loudsTrie/Main.java
+++ src/main/java/com/pakio/loudsTrie/Main.java
@@ -1,5 +1,7 @@
 package com.pakio.loudsTrie;
 
+import com.carrotsearch.sizeof.RamUsageEstimator;
+
 public class Main {
   public static void main(String[] args) {
     Trie trie = new Trie();
@@ -15,5 +17,8 @@ public class Main {
 
     System.out.println(louds.match("one"));
     System.out.println(louds.match("on"));
+
+    System.out.println("Trie size(byte) -> " + RamUsageEstimator.sizeOf(trie));
+    System.out.println("LOUDS size(byte) -> " + RamUsageEstimator.sizeOf(louds));
   }
 }
